2b9704de2da7c379365112c63ff35da25dd5a5da
Riley Williams <46320472+aysalama@users.noreply.github.com>
2019-07-20 02:07:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,24 @@
 # A calculator with premade equations
 # User inputs with an immediate conversion to int
 
-x = int(input('SAT 1: '))
-y = int(input('SAT 2: '))
-z = int(input('GPA: '))
+# While loop to restrict some of the user inputs.
 
-# The designated equations
+while True:
+    try:
+        x = int(input('SAT 1: '))
+        y = int(input('SAT 2: '))
+        z = int(input('GPA: '))  
+        assert 400 <= x <= 1600
+        assert 400 <= y <= 1600
+        assert 10 <= z <= 40
+    except ValueError:
+        print("Not an integer! Please enter an integer.")
+    except AssertionError:
+        print("Please enter an integer for SAT scores between 400 and 1600 and for GPA between 10 and 40.")
+    else:
+        break
+
+# If function starting point for the designated equations
 
 if x >= 1090 and y >=1100:
     e = x / 1600 * 69 + y / 1600 * 15 + z
